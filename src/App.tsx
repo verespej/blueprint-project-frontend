5e7c02b94b8e8363ee0952a74aedff6d44229144
Hakon <mrhakon@gmail.com>
2025-05-13 12:11:37 -0600

    Add coming-soon for educational resources

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,10 @@ import { Landing } from './pages/Landing';
 import { Login } from './pages/Login';
 import { MyPatients } from './pages/MyPatients';
 import { NotFound } from './pages/NotFound';
+import { PatientEducation } from './pages/PatientEducation';
 import { PatientHome } from './pages/PatientHome';
 import { PatientManager } from './pages/PatientManager';
+import { ProviderEducation } from './pages/ProviderEducation';
 import { ProviderHome } from './pages/ProviderHome';
 
 export function App() {
@@ -20,7 +22,9 @@ export function App() {
         <Route element={<DashboardLayout />}>
           <Route path="/my-patients"  element={<MyPatients />} />
           <Route path="/provider" element={<ProviderHome />} />
+          <Route path="/provider-education" element={<ProviderEducation />} />
           <Route path="/patient"  element={<PatientHome />} />
+          <Route path="/patient-education"  element={<PatientEducation />} />
           <Route path="/patient-manager/:patientId" element={<PatientManager />} />
         </Route>
       </Route>
